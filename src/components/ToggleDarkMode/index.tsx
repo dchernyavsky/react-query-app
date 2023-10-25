b1b578af57890f9ae04cebbd6c84de6327848423
dchernyavsky <dchernyavsky678@gmail.com>
2023-10-25 20:14:15 +0600

    refactor: dark mode useReducer

--- src/components/ToggleDarkMode/index.tsx
+++ src/components/ToggleDarkMode/index.tsx
@@ -3,19 +3,20 @@ import { DarkModeSwitch } from "react-toggle-dark-mode";
 
 export const ToggleDarkMode: React.FC = () => {
   const darkModeClassName = "dark";
-  const [isDarkMode, setIsDarkMode] = React.useState(
-    Boolean(window.matchMedia) &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
+  const [isDarkMode, toggleDarkMode] = React.useReducer(
+    (currentState: boolean, newState: boolean) => {
+      if (newState) {
+        document.body.classList.add(darkModeClassName);
+      } else {
+        document.body.classList.remove(darkModeClassName);
+      }
+      return newState;
+    },
+    Boolean(
+      window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+    )
   );
-
-  const toggleDarkMode = (checked: boolean) => {
-    setIsDarkMode(checked);
-    if (checked) {
-      document.body.classList.add(darkModeClassName);
-    } else {
-      document.body.classList.remove(darkModeClassName);
-    }
-  };
 
   React.useEffect(() => {
     toggleDarkMode(isDarkMode);
