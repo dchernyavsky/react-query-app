839881161bb0a7da7b841717763700a50e9af8bd
dchernyavsky <dchernyavsky678@gmail.com>
2023-12-18 22:31:00 +0600

    feat: api url to env

--- src/api/PhotoApi.ts
+++ src/api/PhotoApi.ts
@@ -1,8 +1,10 @@
 import config from "../config.json";
 
+const URL = process.env.REACT_APP_URL || config.URL;
+
 class PhotoApi {
-  private photoUrl = `${config.URL}/photos/`;
-  private photosUrl = `${config.URL}/photos?albumId=`;
+  private photoUrl = `${URL}/photos/`;
+  private photosUrl = `${URL}/photos?albumId=`;
 
   async getPhotos(page: number) {
     const data = await fetch(`${this.photosUrl}${page}`);
